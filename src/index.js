1002c516e54a37770e34cb8882c6f1c8186db0d7
Yuri Vishnevsky <yurivish@gmail.com>
2023-10-05 13:14:00 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,458 @@
+export const DEBUG = true;
+
+export class H2Encoding {
+  /**
+   * H2Encoding encodes values from the range [0, 2^n) into base-2 logarithmic bins
+   * with a configurable error bound.
+   * 
+   * The number of bins must be less than 2^32, and the largest encodable value must
+   * be less than 2^53.
+   * 
+   * @param {object} options
+   * @param {number} options.a - 2^a is the absolute error on the low end
+   * @param {number} options.b - 2^-b is the relative error on the high end
+   * @param {number} options.n? - 2^n-1 is the maximum encodable value (default: 53)
+   * */
+  constructor({ a, b, n = 53 }) {
+    assertSafeInteger(a);
+    assertSafeInteger(b);
+    assertSafeInteger(n);
+    assert(n <= 53, () => `expected n <= 53, got ${n}`);
+
+    const c = a + b + 1;
+    assert(
+      c < 32,
+      () => `expected cutoff parameter c = a + b + 1 <= 32, got ${a + b + 1}`
+    );
+
+    this.a = u32(a);
+    this.b = u32(b);
+    this.n = u32(n);
+
+    // 2^c is the cutoff point between the relative & absolute regions
+    this.c = u32(c);
+
+    // The maxiumum possible bin index (ie. numBins - 1)
+    this.maxCode = u32(H2Encoding.maxCodeForParams({ a, b, n }));
+  }
+
+  /**
+   * Convenience constructor that allows specifying a histogram with more intuitive parameters.
+   * @param {object} options
+   * @param {number} options.relativeError - relative error bound for this histogram, in (0, 1].
+   * @param {number} [options.minimumUnit] - smallest distinguishable unit, below which we do not
+   * care about relative error. Eg. if our data comes as nanoseconds but we only care about
+   * relative error in terms of microseconds, minimumUnit should be set to 1000. (default: 1)
+   * @param {number} [options.maxValue] - maximum encodable value (default: 2^53 - 1)
+   */
+  static with({ relativeError, minimumUnit = 1, maxValue = 2 ** 53 - 1 }) {
+    assert(relativeError > 0 && relativeError <= 1, () => `expected relative error to be in (0, 1], got ${relativeError}`);
+    // Since we use bit shifts to handle the parameters, we need `a` >= 0, so the minimum
+    // unit must be a positive number greater than 1. 
+    // There's no conceptual issue with smaller numbers, but they are hard to support with bit math.
+    assert(minimumUnit >= 1, () => `expected minimumUnit > 1, got ${minimumUnit}`);
+    // Mandate that maxValue is an integer in order to avoid issues with floating-point
+    // rounding, eg. Math.log2(1.0000000000000002 + 1) === 1
+    assert(maxValue >= 1, () => `expected maxValue >= 1, got ${maxValue}`);
+    assertSafeInteger(maxValue); 
+    const a = Math.floor(Math.log2(minimumUnit));
+    let b = -Math.floor(Math.log2(relativeError));
+    // since `2^n` is the first unrepresentable value,
+    // add 1 to maxValue so that we can represent it.
+    const n = Math.ceil(Math.log2(maxValue + 1));
+    return new H2Encoding({ a, b, n });
+  }
+
+  /**
+   * Return the bin index of the value, given this histogram's parameters.
+   * Values can be any number (including non-integers) within the value range.
+   * @param {number} value
+   */
+  encode(value) {
+    // We allow non-integral inputs since JS numbers are 64-bit floats.
+    const { a, b, c } = this;
+    assert(value >= 0 && value <= this.maxValue(), "expected value in histogram range [0, 2^n)");
+
+    if (value < u32(1 << c)) {
+      // We're below the cutoff.
+      // The bin width below the cutoff is 1 << a and we can use a bit shift
+      // to compute the bin since we know the value is less than 2^32.
+      return value >>> a;
+    }
+
+    // We're above the cutoff.
+    // Compute the bin offset by figuring out which log segment we're in,
+    // as well as which bin inside that log segment we're in.
+
+    // The log segment containing the value
+    const v = Math.floor(Math.log2(value));
+
+    // The bin offset within the v-th log segment.
+    // To compute this with bit shifts: (value - u32(1 << v)) >>> (v - b)
+    // - `value - (1 << v)` zeros the topmost (v-th) bit.
+    // - `>>> (v - b)` extracts the top `b` bits of the value, corresponding
+    //   to the bin index within the v-th log segment.
+    //
+    // To account for larger-than-32-bit values, however, we do this without bit shifts:
+    const binsWithinSeg = Math.floor((value - 2 ** v) / 2 ** (v - b));
+    DEBUG && assertSafeInteger(binsWithinSeg);
+
+    // We want to calculate the number of bins that precede the v-th log segment.
+    // 1. The linear section below the cutoff has twice as many bins as any log segment
+    //    above the cutoff, for a total of 2^(b+1) = 2*2^b bins below the cutoff.
+    // 2. Above the cutoff, there are `v - c` log segments before the v-th log segment,
+    //    each with 2^b bins, for a total of (v - c) * 2^b bins above the cutoff.
+    // Taken together, there are (v - c + 2) * 2^b bins preceding the v-th log segment.
+    // Since the number of bins is always less than 2^32, this can be done with bit ops.
+    const binsBelowSeg = u32((2 + v - c) << b);
+
+    return binsBelowSeg + binsWithinSeg;
+  }
+
+  /**
+   * Given a bin index, returns the lowest value that bin can contain.
+   * @param {number} code
+   */
+  lowest(code) {
+    const { a, b, c } = this;
+
+    // There are 2^(c - a) = 2^(b + 1) bins below the cutoff.
+    const binsBelowCutoff = 1 << (c - a);
+    if (code < binsBelowCutoff) {
+      return u32(code << a);
+    } 
+
+    // The number of bins in 0..code that are above the cutoff point
+    const n = code - binsBelowCutoff;
+
+    // The index of the log segment we're in: there are `c` log
+    // segments below the cutoff and `n >> b` above, since each
+    // one is divided into 2^b bins.
+    const seg = c + (n >>> b);
+
+    // By definition, the lowest value in a log segment is 2^seg
+    // do this without bit shifts, since those return a 32-bit signed integer.
+    const segStart = 2 ** seg;
+
+    // The bin we're in within that segment, given by the low bits of n:
+    // the bit shifts remove the `b` lowest bits, leaving only the high
+    // bits, which we then subtract from `n` to keep only the low bits.
+    const bin = n - u32((n >>> b) << b);
+
+    // The width of an individual bin within this log segment (segStart >>> b)
+    const binWidth = Math.floor(segStart / 2 ** b);
+
+    // The lowest value represented by this bin is simple to compute:
+    // start where the logarithmic segment begins, and increment by the
+    // linear bin index within the segment times the bin width.
+    return segStart + bin * binWidth;
+  }
+
+  /**
+   * Given a bin index, returns the least upper bound on the highest value that bin can contain.
+   * For example, if the bin spans the range [0, 4), `highest` will return 4.
+   * @param {number} code
+   */
+  highest(code) {
+    DEBUG && assert(code <= this.maxCode);
+    if (code === this.maxCode) {
+      return this.maxValue();
+    } else {
+      return this.lowest(code + 1);
+    }
+  }
+
+  /**
+   * Return the bin width of the given bin code.
+   * @param {number} code
+   */
+  binWidth(code) {
+    assert(0 <= code && code <= this.maxCode);
+    return this.highest(code) - this.lowest(code) + 1;
+  }
+
+  /**
+   *  Return the maximum value representable by these histogram parameters.
+   */
+  maxValue() {
+    return 2 ** this.n - 1;
+  }
+
+  /**
+   * Absolute error on the low end of the histogram, below the cutoff
+   */
+  absoluteError() {
+    return 2 ** this.a;
+  }
+
+  /** 
+   * Relative error on the high end of the histogram, above the cutoff
+   */
+  relativeError() {
+    return 2 ** -this.b;
+  }
+
+  /**
+   * Transition point below which is relative error and
+   * above which is alsolute error
+   */
+  relativeAbsoluteCutoff() {
+    return 2 ** this.c;
+  }
+
+  /**
+   * Returns the number of bins represented by this encoding.
+   * Note that the result may be 2^32, which exceeds the maximum
+   * representable value of an unsigned 32-bit integer.
+   */
+  numBins() {
+    return this.maxCode + 1;
+  }
+
+  /**
+   * Return the maximum bin index for the given {a, b, n} parameters.
+   * @param {{ a: number, b: number, n?: number }} options
+   * */
+  static maxCodeForParams({ a, b, n = 53 }) {
+    const c = a + b + 1;
+    // todo: should this check that the number of bins is a safe integer?
+    if (n < c) {
+      // Each log segment is covered by bins of width 2^a and there are n log segments,
+      // giving us 2^(n - a) bins in total. Also, we always maintain a minimum of 1 bin.
+      return 2 ** Math.max(n - a, 0) - 1;
+    } else {
+      // See the comment in `encode` about `binsBelowSeg` for a derivation of this expression
+      return (2 + n - c) * 2 ** b - 1;
+    }
+  }
+}
+
+export class H2HistogramBuilder {
+  /**
+   * @param {H2Encoding} encoding
+   */
+  constructor(encoding) {
+    // Use a Float64Array to permit counts up to 2^53.
+    this.counts = new Float64Array(encoding.numBins());
+    this.encoding = encoding;
+  }
+
+  /**
+   * Increment the bin containing `value` by `count`.
+   * @param {number} value
+   */
+  increment(value, count = 1) {
+    const bin = this.encoding.encode(value);
+    this.counts[bin] += count;
+  }
+
+  build() {
+    // Sparsify by storing only the nonzero bins
+    const bins = [];
+    const counts = this.counts;
+    for (let i = 0; i < counts.length; i++) {
+      const count = counts[i];
+      if (count > 0) {
+        counts[bins.length] = count;
+        bins.push(i);
+      }
+    }
+    return new H2Histogram(this.encoding, bins, counts.subarray(0, bins.length));
+  }
+}
+
+/**
+ * Sparse histogram representation storing nonzero bins and their counts.
+ */
+export class H2Histogram {
+  /**
+   * @param {H2Encoding} encoding
+   * @param {number[] | Uint32Array} bins
+   * @param {number[] | Float64Array} counts
+   */
+  constructor(encoding, bins, counts) {
+    assert(bins.length === counts.length);
+    // todo: assert no duplicates - or are duplicates fine (if inefficient)?
+    // todo: could (should) we re-use the counts array?
+    const cumulativeCounts = new Float64Array(counts);
+    for (let i = 1; i < cumulativeCounts.length; i++) {
+      cumulativeCounts[i] += cumulativeCounts[i - 1];
+    }
+    this.bins = bins;
+    this.cumulativeCounts = cumulativeCounts;
+    this.encoding = encoding;
+    this.numObservations =
+      counts.length === 0 ? 0 : this.cumulativeCounts[this.cumulativeCounts.length - 1];
+  }
+
+  /**
+   * Return an upper bound on the number of observations at or below `value`.
+   * @param {number} value
+   */
+  cumulativeCount(value) {
+    if (this.numObservations === 0) {
+      return 0;
+    }
+
+    if (value > this.encoding.maxValue()) {
+      return this.numObservations;
+    }
+
+    // The index of the bin containing `value`.
+    // We want to know the count up to and including this bin,
+    // but not including any subsequent bins.
+    const bin = this.encoding.encode(value);
+
+    // The number of observations that are in or below that bin.
+    // `i` tells us the index of the first bin above the bin containing `value`.
+    const i = partitionPoint(this.bins.length, (i) => this.bins[i] <= bin);
+
+    // We want the count from the bin before that one.
+    return i === 0 ? 0 : this.cumulativeCounts[i - 1];
+  }
+
+  /**
+   * Return an upper bound on the fraction of observations at or below `value` .
+   * Like cumulative_count, but returns the fraction of the data rather than a count.
+   * @param {number} value
+   */
+  cdf(value) {
+    if (this.numObservations === 0) {
+      return 1.0;
+    }
+    return this.cumulativeCount(value) / this.numObservations;
+  }
+
+  /**
+   * Return an upper bound on the value of the q-th quantile.
+   * Returns zero if the histogram contains no observations.
+   * @param {number} q - the quantile, in [0, 1]
+   */
+  quantile(q) {
+    DEBUG && assert(0 <= q && q <= 1);
+
+    if (this.numObservations === 0) {
+      return 0;
+    }
+
+    // Number of observations at or below the q-th quantile
+    const k = this.quantileToCount(q);
+    // this.bins[i] is the index of the bin containing the k-th observation.
+    // There are two levels of indexing here, since `bins` itself contains "indices"
+    const i = Math.min(
+      partitionPoint(
+        this.cumulativeCounts.length,
+        (i) => this.cumulativeCounts[i] < k
+      ),
+      this.cumulativeCounts.length - 1
+    );
+
+    // Maximum value in that bin
+
+    return this.encoding.highest(this.bins[i]);
+  }
+
+  /**
+   * Return an upper bound in [1, count] on the number of observations that lie
+   * at or below the q-th quantile. E.g. if there are 2 observations,
+   * - quantile_to_count(0) == 0
+   * - quantile_to_count(0.25) == 1,
+   * - quantile_to_count(0.75) == 2
+   * - quantile_to_count(1.0) == 2
+  /**
+   * @param {number} q
+   */
+  quantileToCount(q) {
+    DEBUG && assert(0.0 <= q && q <= 1.0);
+    if (q == 0.0) {
+      return 1;
+    }
+    return Math.ceil(q * this.numObservations);
+  }
+}
+
+/**
+ * 
+ * @param {boolean} condition
+ * @param {string | (() => string) } [message] - error message as a string or zero-argument function, 
+ * to allow deferring the evaluation of an expensive message until the time an error occurs.
+ */
+function assert(condition, message) {
+  const prefix = 'assertion error';
+  if (condition !== true) {
+    const text = typeof message === "function" ? message() : message;
+    throw new Error(text === undefined ? prefix : `${prefix}: ${text}`);
+  }
+};
+
+/**
+ * @param {number} x
+ */
+function assertSafeInteger(x) {
+  assert(Number.isSafeInteger(x), () => `expected safe integer, got ${x}`);
+}
+
+/**
+ * @param {any} x
+ */
+function assertDefined(x) {
+  assert(x !== undefined, 'expected a defined value, got undefined');
+};
+
+/**
+ * Returns the largest index for which `pred` returns true, plus one.
+ * If the predicate does not return true for any index, returns 0.
+ * The predicate function `pred` is required to be monotonic, ie. 
+ * to return `true` for all inputs below some cutoff, and `false`
+ * for all inputs above that cutoff.
+ * 
+ * This implementation is adapted from https://orlp.net/blog/bitwise-binary-search/
+ * 
+ * That post contains optimized versions of this function, but here I opted for the
+ * clearest implementation, at a slight performance cost.
+ * 
+ * @param {number} n
+ * @param {(index: number) => boolean} pred
+ */
+function partitionPoint(n, pred) {
+  DEBUG && assert(n < 2 ** 32);
+  DEBUG && assertSafeInteger(n);
+  let b = 0;
+  let bit = bitFloor(n);
+  while (bit !== 0) {
+    const i = ((b | bit) - 1) >>> 0;
+    if (i < n && pred(i)) {
+      b |= bit;
+    }
+    bit >>>= 1;
+  }
+  return b >>> 0;
+}
+
+/**
+ * If x is not zero, calculates the largest integral power of two that is not greater than x.
+ * If x is zero, returns zero.
+ * Like the function in the C++ standard library: https://en.cppreference.com/w/cpp/numeric/bit_floor
+ * @param {number} n
+ */
+function bitFloor(n) {
+  DEBUG && assert(n < 2 ** 32);
+  if (n === 0) {
+    return 0;
+  }
+  const msb = 31 - Math.clz32(n);
+  return (1 << msb) >>> 0;
+}
+
+/**
+ * Coerces x to an unsigned 32-bit unsigned integer. This is provided as
+ * a convenience function on top of unsigned shift that does some sanity
+ * checks in debug mode.
+ * @param {number} x
+ */
+function u32(x) {
+  DEBUG && assert(Number.isInteger(x));
+  // Allow bit patterns representing negative numbers, eg. 1 << 31
+  DEBUG && assert(Math.abs(x) < 2 ** 32, () => `expected x < 2^32, got ${x}`);
+  return x >>> 0;
+}
---
+++ src/index.test.js
@@ -0,0 +1,210 @@
+import * as fc from 'fast-check';
+import { describe, expect, it, test } from 'vitest';
+import { H2Encoding, H2Histogram, H2HistogramBuilder } from './index.js';
+
+// todo: is relative error < or <= 2^-b?
+
+describe('H2Encoding', () => {
+  test('H2Encoding.with', () => {
+    const enc = H2Encoding.with({ minimumUnit: 1, relativeError: 0.01, maxValue: 1e6 });
+    expect(enc.a).toBe(0);
+    expect(enc.b).toBe(7);
+    expect(enc.n).toBe(20);
+
+
+    fc.assert(fc.property(
+      fc.double({ min: 1, max: 10, noNaN: true, }), // minimum unit
+      // @ts-ignore
+      fc.double({ min: 0.0001, max: 1, noNaN: true, }), // relative error
+      fc.integer({ min: 1, max: enc.maxValue() }), // maximum value
+      (minimumUnit, relativeError, maxValue) => {
+        const enc = H2Encoding.with({ minimumUnit, relativeError, maxValue });
+        expect(enc.absoluteError()).toBeLessThanOrEqual(minimumUnit);
+        expect(enc.relativeError()).toBeLessThanOrEqual(relativeError);
+        expect(enc.maxValue()).toBeGreaterThanOrEqual(maxValue);
+      })
+    );
+  }); 
+
+  test('H2Encoding.encode', () => {
+    let enc = new H2Encoding({ a: 1, b: 2, n: 6 });
+    let bins = [
+      // note: the last 2 values, currently 15, would be 16 if n was greater than 6.
+      0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10,
+      10, 10, 11, 11, 11, 11, 12, 12, 12, 12, 12, 12, 12, 12, 13, 13, 13, 13, 13,
+      13, 13, 13, 14, 14, 14, 14, 14, 14, 14, 14, 15, 15, 15, 15, 15, 15, 15, 15
+    ];
+    for (let i = 0; i < bins.length; i++) {
+      const bin = bins[i];
+      expect(enc.encode(i)).toBe(bin);
+    }
+    expect(() => enc.encode(bins.length + 1)).toThrow();
+  });
+
+  test('Values are encoded into the bin that contains them', () => {
+    /**
+     * @param {H2Encoding} enc
+     */
+    function propertyTest(enc) {
+      // 
+      fc.assert(fc.property(
+        fc.double({ min: 0, max: enc.maxValue(), noNaN: true, }),
+        // @ts-ignore
+        value => {
+          const code = enc.encode(value);
+          const lowest = enc.lowest(code);
+          const highest = enc.highest(code);
+          expect(lowest <= value && value <= highest).toBe(true);
+        }
+      ));
+    }
+
+    propertyTest(new H2Encoding({ a: 0, b: 7, n: 20 }));
+    propertyTest(new H2Encoding({ a: 0, b: 0, n: 0 }));
+    propertyTest(new H2Encoding({ a: 0, b: 0, n: 53 }));
+    propertyTest(new H2Encoding({ a: 20, b: 0, n: 53 }));
+    propertyTest(new H2Encoding({ a: 0, b: 20, n: 53 }));
+    propertyTest(new H2Encoding({ a: 0, b: 20, n: 21 }));
+
+    for (let a = 0; a < 5; a++) {
+      for (let b = 0; b < 5; b++) {
+        propertyTest(new H2Encoding({ a, b, n: 11 }));
+      }
+    }
+  });
+
+  test('H2Encoding.numBins', () => {
+    expect(new H2Encoding({ a: 0, b: 0, n: 0 }).numBins()).toBe(1);
+    expect(new H2Encoding({ a: 0, b: 0, n: 6 }).numBins()).toBe(7);
+    expect(new H2Encoding({ a: 0, b: 0, n: 7 }).numBins()).toBe(8);
+    expect(new H2Encoding({ a: 0, b: 2, n: 6 }).numBins()).toBe(20);
+    expect(new H2Encoding({ a: 1, b: 2, n: 6 }).numBins()).toBe(16);
+    expect(new H2Encoding({ a: 1, b: 0, n: 6 }).numBins()).toBe(6);
+    expect(new H2Encoding({ a: 1, b: 1, n: 6 }).numBins()).toBe(10);
+    expect(new H2Encoding({ a: 0, b: 1, n: 6 }).numBins()).toBe(12);
+    expect(new H2Encoding({ a: 2, b: 0, n: 4 }).numBins()).toBe(3);
+    expect(new H2Encoding({ a: 2, b: 1, n: 4 }).numBins()).toBe(4);
+    expect(new H2Encoding({ a: 2, b: 2, n: 6 }).numBins()).toBe(12);
+    expect(new H2Encoding({ a: 2, b: 2, n: 5 }).numBins()).toBe(8);
+    expect(new H2Encoding({ a: 2, b: 2, n: 4 }).numBins()).toBe(4);
+    expect(new H2Encoding({ a: 2, b: 3, n: 3 }).numBins()).toBe(2);
+    expect(new H2Encoding({ a: 2, b: 3, n: 2 }).numBins()).toBe(1);
+    expect(new H2Encoding({ a: 2, b: 3, n: 1 }).numBins()).toBe(1);
+    expect(new H2Encoding({ a: 2, b: 3, n: 0 }).numBins()).toBe(1);
+
+    // 2 bins in the linear segment, 1 in the log segment
+    expect(new H2Encoding({ a: 30, b: 0, n: 32 }).numBins()).toBe(3);
+
+    // 2 bins in the linear segment, 22 in the log segment
+    expect(new H2Encoding({ a: 30, b: 0, n: 53 }).numBins()).toBe(24);
+  });
+
+  // c = a + b + 1 out of bounds (above 31)
+  expect(() => new H2Encoding({ a: 20, b: 20, n: 53 })).toThrow();
+  
+  // n out of bounds
+  expect(() => new H2Encoding({ a: 0, b: 0, n: 54 })).toThrow();
+});
+
+test('H2Histogram', () => {
+  {
+    // Basic tests with a 3-value histogram containing [1e5, 2e5, 3e5]
+    const enc = H2Encoding.with({ relativeError: 0.01, maxValue: 1e6 });
+    const builder = new H2HistogramBuilder(enc);
+    builder.increment(1e5, 100);
+    builder.increment(2e5, 100);
+    builder.increment(3e5, 100);
+    const hist = builder.build();
+    expect(hist.numObservations).toBe(300);
+    expect(hist.cumulativeCount(100)).toBe(0);
+    expect(hist.quantile(0)).toBeGreaterThan(1e5);
+    expect(hist.quantile(0)).toBeLessThan(1.01 * 1e5);
+    expect(hist.quantile(0.5)).toBeGreaterThan(2e5);
+    expect(hist.quantile(0.5)).toBeLessThan(1.01 * 2e5);
+    expect(hist.quantile(1)).toBeGreaterThanOrEqual(3e5);
+    expect(hist.quantile(1)).toBeLessThan(1.01 * 3e5);
+
+    expect(hist.cdf(0.999 * 1e5)).toBe(1 / 3);
+    expect(hist.cdf(1e5)).toBe(1 / 3);
+    expect(hist.cdf(1.1e5)).toBe(1 / 3);
+    expect(hist.cdf(1.5e5)).toBe(1 / 3);
+
+    expect(hist.cdf(2e5)).toBe(2 / 3);
+    expect(hist.cdf(2.1e5)).toBe(2 / 3);
+    expect(hist.cdf(2.5e5)).toBe(2 / 3);
+    expect(hist.cdf(2.9e5)).toBe(2 / 3);
+    expect(hist.cdf(2.999e5)).toBe(3 / 3);
+
+    expect(hist.cdf(3e5)).toBe(3 / 3);
+    expect(hist.cdf(5e5)).toBe(3 / 3);
+
+    expect(hist.cdf(1e9)).toBe(3 / 3);
+
+    expect(hist.quantile(0.25)).toBeGreaterThan(1e5);
+    expect(hist.quantile(0.75)).toBeGreaterThan(2e5);
+    expect(hist.quantile(1)).toBeGreaterThan(3e5);
+  }
+
+  {
+
+    // uses the nearest-rank method:
+    // https://en.wikipedia.org/wiki/Percentile#The_nearest-rank_method
+    // todo: til
+    /**
+     * @param {Float64Array} values
+     * @param {number} q
+     */
+    function nearestRankQuantile(values, q) {
+      if (values.length === 0) {
+        return 0;
+      }
+      return values[(q === 0 ? 1 : Math.ceil(q * values.length)) - 1];
+    }
+
+    // Property test that quantiles are retrieved within the expected margin of error (compute-intensive)
+    console.clear();
+    fc.assert(
+      fc.property(
+        fc.double({ min: 0.01, max: 1, minExcluded: true, noNaN: true }), // relativeError
+        // @ts-ignore
+        fc.integer({ min: 1, max: 2 ** 53 - 1 }),
+        (relativeError, maxValue) => {
+          const enc = H2Encoding.with({ relativeError, maxValue });
+          const cutoff = enc.relativeAbsoluteCutoff();
+          const absoluteError = enc.absoluteError();
+          fc.assert(
+            fc.property(
+              fc.float64Array({
+                min: 0,
+                max: enc.maxValue(),
+                noNaN: true,
+                size: "xlarge"
+              }),
+              // @ts-ignore
+              (values) => {
+                values.sort();
+                const builder = new H2HistogramBuilder(enc);
+                for (const value of values) {
+                  builder.increment(value);
+                }
+                const hist = builder.build();
+
+                for (let i = 0; i < 100; i++) {
+                  const q = Math.random(); // note: will never generate 1.0
+                  const baseline = nearestRankQuantile(values, q);
+                  const ours = hist.quantile(q);
+                  if (baseline < cutoff) {
+                    expect(Math.abs(ours - baseline)).toBeLessThanOrEqual(absoluteError);
+                  } else {
+                    expect(Math.abs(baseline - ours) / baseline).toBeLessThanOrEqual(relativeError);
+                  }
+                }
+              }
+            ),
+            // Run less iterations of this assert since it is nested inside another assert
+            { numRuns: 2 });
+        }
+      )
+    );
+  }
+});
